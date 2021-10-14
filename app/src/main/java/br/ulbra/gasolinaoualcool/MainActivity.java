bcf688bd66d4ce99944f9af873d31bec64a7a94f
Jeferson Leon <jefersonleonblue@gmail.com>
2021-10-13 21:48:43 -0300

    Aula 13/10 - Projeto Gasolina ou Alcool - Tela implemantada. Programação resolvida no Java - Com alteração do Icone

--- app/src/main/java/br/ulbra/gasolinaoualcool/MainActivity.java
+++ app/src/main/java/br/ulbra/gasolinaoualcool/MainActivity.java
@@ -3,12 +3,49 @@ package br.ulbra.gasolinaoualcool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+   private EditText edtAlcool, edtGasolina;
+   private Button btnCompara;
+   private TextView txtResult1, txtResult2;
+   public double valorAl, valorGas, percentual, resultado;
+   public  String textoResult = "";
+   DecimalFormat f = new  DecimalFormat("0.00");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();//oculta o titulo da aplicação
+        iniciarComponentes();//inicializado os componentes
+        /* clique do botão verificar */
+        btnCompara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorAl = Double.parseDouble(edtAlcool.getText().toString());
+                valorGas = Double.parseDouble(edtGasolina.getText().toString());
+                percentual = valorAl/valorGas * 100;
+                txtResult1.setText(f.format(percentual)+"%");
+                if(percentual > 73){
+                    textoResult = "Opte por abastecer com Gasolina";
+                }else{
+                    textoResult = "Opte por abastecer com Álcool";
+                }
+                txtResult2.setText(textoResult);
+            }
+        });
+    }
+
+    private void iniciarComponentes() {
+        edtAlcool = findViewById(R.id.edtAlcool);
+        edtGasolina = findViewById(R.id.edtGasolina);
+        btnCompara = findViewById(R.id.btnVerificar);
+        txtResult1 = findViewById(R.id.txtResutado1);
+        txtResult2 = findViewById(R.id.txtResultado2);
     }
 }
